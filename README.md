2115d9851181f5f29ea5a2525ad2576fa4b79357
tlesh989 <thomas.lesh@gmail.com>
2023-02-16 17:07:35 -0500

    tailscale lxc container

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ export OP_ACCOUNT=1PasswordAccount
 export PM_USER=ProxmoxUsername
 export PM_PASS=ProxmoxPassword
 export TF_TOKEN_app_terraform_io=TerraformCloudToken
+export TF_VAR_root_pass=RootUserPassword
 ```
--- terraform/tailscale.tf
+++ terraform/tailscale.tf
@@ -1,10 +1,10 @@
-resource "proxmox_lxc" "unifi" {
+resource "proxmox_lxc" "tailscale" {
   target_node  = "huma"
-  hostname     = "unifi"
+  hostname     = "tailscale"
   ostemplate   = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
   password     = var.root_pass
   memory       = 2048
-  unprivileged = true
+  unprivileged = false
   onboot       = true
   start        = true
 
@@ -19,7 +19,6 @@ resource "proxmox_lxc" "unifi" {
   network {
     name   = "eth0"
     bridge = "vmbr0"
-    ip     = "192.168.233.5/24"
-    gw     = "192.168.233.1"
+    ip     = "dhcp"
   }
 }
