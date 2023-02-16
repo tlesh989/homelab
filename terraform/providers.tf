5e8715b2556ac68922b67141923d3298b292f6a3
tlesh989 <thomas.lesh@gmail.com>
2023-02-16 16:38:04 -0500

    proxmox unifi container

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,7 +16,14 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url = "https://192.168.233.6:8006/api2/json"
+  pm_api_url    = "https://192.168.233.6:8006/api2/json"
+  pm_log_enable = true
+  pm_log_file   = "terraform-plugin-proxmox.log"
+  pm_debug      = true
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
+  }
 }
 
 # pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Monitor VM.PowerMgmt"
--- terraform/unifi.tf
+++ terraform/unifi.tf
@@ -1,8 +1,9 @@
 resource "proxmox_lxc" "unifi" {
   target_node  = "huma"
   hostname     = "unifi"
-  ostemplate   = "local:vztmpl/ubuntu-22.04-1-_amd64.tar.zst"
+  ostemplate   = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
   password     = var.root_pass
+  memory       = 2048
   unprivileged = true
   onboot       = true
 
@@ -18,5 +19,6 @@ resource "proxmox_lxc" "unifi" {
     name   = "eth0"
     bridge = "vmbr0"
     ip     = "192.168.233.5/24"
+    gw     = "192.168.233.1"
   }
 }
