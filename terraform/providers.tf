d46d2a3bde3caabc50e17c3cb442a940b6b1d9e8
tlesh989 <thomas.lesh@gmail.com>
2023-03-25 22:53:46 -0400

    fixed

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -24,14 +24,13 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url    = "https://192.168.233.6:8006/api2/json"
+  pm_api_url = "https://192.168.233.6:8006/api2/json"
   # pm_log_enable = true
   # pm_log_file   = "terraform-plugin-proxmox.log"
   # pm_debug      = true
   # pm_log_levels = {
   #   _default    = "debug"
   #   _capturelog = ""
-  }
 }
 
 provider "cloudflare" {
