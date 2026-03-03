7c1b548db85777d75d543f920900e009488503a1
Tom Lesh <thomas.lesh@gmail.com>
2026-03-03 18:40:11 -0500

    feat: Integrate Doppler for secret management, removing direct variable inputs for Proxmox API credentials and updating CI/CD workflows.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,14 +3,3 @@ variable "default_node_name" {
   type        = string
   default     = "bupu"
 }
-
-variable "pm_api_user" {
-  description = "The username for the Proxmox VE API."
-  type        = string
-}
-
-variable "pm_api_password" {
-  description = "The password for the Proxmox VE API."
-  type        = string
-  sensitive   = true
-}
